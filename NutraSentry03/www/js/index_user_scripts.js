fc19e2b9ccf5f487bcc90255402ae1de9b67db6e
Erica Boyer <boyer.373@osu.edu>
2014-11-15 14:58:07 -0500

    added dummy search page

--- NutraSentry03/www/js/index_user_scripts.js
+++ NutraSentry03/www/js/index_user_scripts.js
@@ -27,10 +27,46 @@
          activate_subpage("#addrestriction"); 
     });
     
+        /* list item  Placeholder Template */
+    $(document).on("click", ".uib_w_10", function(evt)
+    {
+         activate_subpage("#testpage"); 
+    });
+    
+        /* button  bCheckFood */
+    $(document).on("click", "#bCheckFood", function(evt)
+    {
+         activate_subpage("#foodsearch"); 
+    });
+    
+        /* button  bSearchForRecipe */
+    $(document).on("click", "#bSearchForRecipe", function(evt)
+    {
+         activate_subpage("#foodsearch"); 
+    });
+    
+        /* list item  Result1 */
+    $(document).on("click", ".uib_w_25", function(evt)
+    {
+         activate_subpage("#testpage"); 
+    });
+    
+        /* list item  Result2 */
+    $(document).on("click", ".uib_w_26", function(evt)
+    {
+         activate_subpage("#testpage"); 
+    });
+    
+        /* list item  Result3 */
+    $(document).on("click", ".uib_w_27", function(evt)
+    {
+         activate_subpage("#testpage"); 
+    });
+    
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
-
+/*
 function getUserName() {
     return document.getElementById(iUserName).value.toString;
 }
@@ -42,3 +78,11 @@ function displayGreeting() {
 function addToRestrictionsList(eName) {
     restrictionsList[restrictionsList.length] = document.getElementById(eName).label;
 }
+
+function generateRestrictionsList() {
+    for (i = 0; i < restrictionsList.length; i++) {
+        var n = restrictionsList[i].label;
+        listText += "<li class="topcoat-list__item widget uib_w_10" data-uib="topcoat/list_item" data-ver="0">" + n + "</li>\n"
+    }
+}
+*/
